fcee4a8c1f2358c6247b5f47885dcedf85cd90a0
Kenji Isak Laguan <120000674+kenjisak@users.noreply.github.com>
2023-11-22 19:04:02 -0500

    CODE-001: added multiple web drivers that registers each player

--- COMP4004-CrazyEightsGame-main/src/test/java/com/example/comp4004f22a3101077008/AcceptanceTest.java
+++ COMP4004-CrazyEightsGame-main/src/test/java/com/example/comp4004f22a3101077008/AcceptanceTest.java
@@ -1,10 +1,21 @@
 package com.example.comp4004f22a3101077008;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
+
+import java.time.Duration;
 
 
 @DirtiesContext
@@ -18,4 +29,26 @@ public class AcceptanceTest {
     @LocalServerPort
     int port;
 
+    @Test
+    public void testGameDrivers(){
+        SpringApplication.run(Application.class);
+
+        System.setProperty("webdriver.chrome.driver",
+                ".\\chromedriver-win64\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver[] multiDriver = new WebDriver[4];
+        for (int i = 0; i < multiDriver.length; i++) {
+            multiDriver[i] = new ChromeDriver(options);
+            multiDriver[i].get("http://localhost:" + port);
+
+            WebDriverWait wait = new WebDriverWait(multiDriver[i], Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.elementToBeClickable (By.id("usernameBtn"))).click();//waits till register button pops up before closing
+        }
+
+        for (WebDriver webDriver : multiDriver) {//close all drivers after
+            webDriver.close();
+        }
+    }
 }
