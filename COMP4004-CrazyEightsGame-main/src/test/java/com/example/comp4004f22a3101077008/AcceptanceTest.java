418c58cc1efccf0f69fb3f315bb18790ed65794d
Kenji Isak Laguan <120000674+kenjisak@users.noreply.github.com>
2023-12-02 23:46:26 -0500

    CODE-007: Finished testRow28 and windows are moved to left monitor

--- COMP4004-CrazyEightsGame-main/src/test/java/com/example/comp4004f22a3101077008/AcceptanceTest.java
+++ COMP4004-CrazyEightsGame-main/src/test/java/com/example/comp4004f22a3101077008/AcceptanceTest.java
@@ -2,6 +2,7 @@ package com.example.comp4004f22a3101077008;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -49,25 +50,25 @@ public class AcceptanceTest {
         for (int i = 0; i < allDrivers.length; i++) {
             allDrivers[i] = new ChromeDriver(options);
             allDrivers[i].get("http://localhost:" + port);
-
-            int screenWidth = 1920/2;
-            int screenHeight = 1080/2;
+            //arranges and moves windows to left screen
+            int screenWidth = 1920;
+            int screenHeight = 1080;
             int x,y;
             if (i == 0){
-                x = 0;
+                x = -screenWidth;//0
                 y = 0;
             } else if (i == 1) {
-                x = screenWidth;
+                x = screenWidth/2 - screenWidth;//screenWidth/2
                 y = 0;
             } else if (i == 2) {
-                x = 0;
-                y = screenHeight;
+                x = -screenWidth;//0
+                y = screenHeight/2;
             }else{
-                x = screenWidth;
-                y = screenHeight;
+                x = screenWidth/2 - screenWidth;//screenWidth/2
+                y = screenHeight/2;
             }
             allDrivers[i].manage().window().setPosition(new org.openqa.selenium.Point(x, y));
-            allDrivers[i].manage().window().setSize(new org.openqa.selenium.Dimension(screenWidth, screenHeight));
+            allDrivers[i].manage().window().setSize(new org.openqa.selenium.Dimension(screenWidth/2, screenHeight/2));
 
             WebDriverWait wait = new WebDriverWait(allDrivers[i], Duration.ofSeconds(20));
             wait.until(ExpectedConditions.elementToBeClickable (By.id("usernameBtn"))).click();//waits till register button pops up then click
@@ -82,6 +83,8 @@ public class AcceptanceTest {
     }
     @Test
     @DirtiesContext
+    @DisplayName("Test Row 25: Next Player")
+    //p1 plays 3C: assert next player is player 2
     public void testRow25() throws InterruptedException {//p1 plays 3C: assert next player is player 2
         rigTestRow25();//rigs deck for this test
 
@@ -120,6 +123,7 @@ public class AcceptanceTest {
     }
     @Test
     @DirtiesContext
+    @DisplayName("Test Row 27: Direction Reversal with Ace")
     //p1 plays 1H assert next player is player 4 AND interface must show now playing in opposite direction (i.e., going right)
     //player 4 plays 7H and next player is player 3
     public void testRow27() throws InterruptedException {
@@ -172,6 +176,46 @@ public class AcceptanceTest {
 
             String playerTurn = allDrivers[i].findElement(By.id("turnID")).getText();
             assertEquals("Turn: 3",playerTurn);//check all players windows they display player 3 as current turn
+
+            WebElement drawBtn = allDrivers[i].findElement(By.id("draw"));
+            if (i == 2){
+                assertTrue(drawBtn.isEnabled());
+            }//assert only player 3 draw button is enabled
+            else{
+                assertFalse(drawBtn.isEnabled());
+            }
+        }
+    }
+
+    @Test
+    @DirtiesContext
+    @DisplayName("Test Row 28: Player turn Skipped with Queen")
+    //p1 plays QC assert next player is player 3  (because player 2 is notified and loses their turn)
+    public void testRow28() throws InterruptedException{
+        rigTestRow28();//rigs deck for this test
+
+        WebDriverWait wait = new WebDriverWait(allDrivers[0], Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.elementToBeClickable (By.id("startBtn"))).click();//waits till start button pops up and starts the game with the rigged deck
+
+        verifyDeckCount();
+
+        TimeUnit.SECONDS.sleep(3);//slow down to see gameplay
+
+        for (WebDriver playerBrowser : allDrivers) {//check all players windows they display the correct starting top card
+            String topCard = playerBrowser.findElement(By.className("topCard")).getAttribute("id");
+            assertEquals("5C",topCard);
+        }
+
+        allDrivers[0].findElement(By.id("QC")).click();//P1 plays QC
+        TimeUnit.SECONDS.sleep(3);//slow down to see gameplay
+
+        for (int i = 0; i < allDrivers.length; i++) {
+
+            String topCard = allDrivers[i].findElement(By.className("topCard")).getAttribute("id");
+            assertEquals("QC",topCard);//check all players windows they display the correct starting top card
+
+            String playerTurn = allDrivers[i].findElement(By.id("turnID")).getText();
+            assertEquals("Turn: 3",playerTurn);//check all players windows they display player 3 as current turn since player 2 was skipped
 
             WebElement drawBtn = allDrivers[i].findElement(By.id("draw"));
             if (i == 2){
@@ -241,6 +285,33 @@ public class AcceptanceTest {
 
         ArrayList<Card> p4Cards = createCards(p4Card);
         gd.getCards().add(16,p4Cards.get(0));
+
+        gd.setTopCard(game.startSetTopCard(gd.getCards()));//set the top card
+
+        for (Player p : gd.getPlayers()) {//deal all players cards
+            game.startDealCards(gd.getCards(), gd.getPlayers(), p.getID() - 1);
+        }
+
+    }
+    public void rigTestRow28(){
+        String topCard = "5C";
+        String p1Card = "QC";
+
+        String rig = "AH 2H 3H 4H 5H 6H 7H 8H 9H TH JH QH KH AS 2S 3S 4S 5S 6S 7S 8S 9S TS JS QS KS AC 2C 3C 4C 5C 6C 7C 8C 9C TC JC QC KC AD 2D 3D 4D 5D 6D 7D 8D 9D TD JD QD KD";//populated deck
+        rig = removeCard(rig,topCard);//remove cards we want to rig
+        rig = removeCard(rig,p1Card);
+        assertTrue(!rig.contains(topCard) && !rig.contains(p1Card));
+
+        ArrayList<Card> gameDeck = createCards(rig);
+        gd.setCards(gameDeck);//setCards with populated Deck
+        game.shuffleDeck(gd.getCards());//shuffle the deck, simulate more realism
+
+        //add top card back
+        gd.getCards().add(0, createCards(topCard).get(0));
+
+        //add back player rigged cards in correct spots
+        ArrayList<Card> p1Cards = createCards(p1Card);
+        gd.getCards().add(1,p1Cards.get(0));
 
         gd.setTopCard(game.startSetTopCard(gd.getCards()));//set the top card
 
